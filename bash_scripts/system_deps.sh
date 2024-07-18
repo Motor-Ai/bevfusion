f076f13c7db9c30e7b719b39af3c50d23423215f
porosh014 <porosh.chowdhurey@gmail.com>
2024-07-18 01:05:08 +0000

    ëxperimental changes for sagemaker

--- bash_scripts/system_deps.sh
+++ bash_scripts/system_deps.sh
@@ -1,3 +1,7 @@
-sudo apt-get install libgl1-mesa-glx libglib2.0-0 -y
-sudo apt-get install openmpi-bin openmpi-common libopenmpi-dev libgtk2.0-dev git -y
+# apt-get install libgl1-mesa-glx libglib2.0-0 -y
+# apt-get install openmpi-bin openmpi-common libopenmpi-dev libgtk2.0-dev git -y
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda install gtk3
+conda install openmpi
 pip install --force-reinstall pip==23.1.2
--- setup_lidar_only_pipeline.py
+++ setup_lidar_only_pipeline.py
@@ -4,8 +4,8 @@ import subprocess
 if __name__ == '__main__':
     subprocess.run(["bash", "bash_scripts/system_deps.sh"])
     subprocess.run(["bash", "bash_scripts/torch_deps.sh"])
-    subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    # subprocess.run(["pip", "install", "-r", "requirements.txt"])
     subprocess.run(["bash", "bash_scripts/source_deps.sh"])
     subprocess.run(["bash", "bash_scripts/openmim_deps.sh"])
-    subprocess.run(["python", "setup.py", "develop"])
+    subprocess.run(["python", "mmdet3d_setup.py", "develop"])
     subprocess.run(["bash", "bash_scripts/train_lidar_only.sh"])
